511972da52fd78d8200c6e0716e63f9f1973b4e2
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-05-04 12:58:23 +0000

    fix github errors

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import ToDoList from "./modules/todolist.js";
-import "./style.css";
-
-const todoList = new ToDoList();
-todoList.addToDo("Wash my car");
-
-const todoListElement = document.querySelector(".todo-list");
-todoListElement.innerHTML = todoList.renderToDos();
