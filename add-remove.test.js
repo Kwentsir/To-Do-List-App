c2c94332ea0948f0d5ea1d09684d486ea250e96b
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-05-12 09:08:44 +0000

    test adding task

--- add-remove.test.js
+++ add-remove.test.js
@@ -2,9 +2,19 @@ import ToDoList from './src/modules/todolist.js';
 
 
 describe("adding element to the list", () => {
-  let ToDoList;
-  test("add element to the list", () => {
-    document.body.innerHTML = "<div>" + '  <ul id="list"></li>' + "</div>";
-    ToDoList(ToDo);
-  });
+  let toDoList;
+  beforeEach (() => {
+ toDoList = new ToDoList ();
+  }),
+    document.body.innerHTML = "<div class='todo-list'></div>";
+    localStorage.clear();
 });
+
+test('should add a new task to the list', () =>{
+  toDoList.addToDo('test');
+  document.querySelector('.todo-list').innerHTML = toDoList.renderToDos();
+const list = document.querySelectorAll('.todo-list .container');
+expect(list).toHaveLength(1);
+});
+
+
