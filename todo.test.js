f4b740ccaad283c308fc709e74ca6a995511df33
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-05-12 09:42:54 +0000

    test removing task from list

--- todo.test.js
+++ todo.test.js
@@ -8,15 +8,22 @@ describe("Adding and removing tasks from the list", () => {
   let toDoList;
   beforeEach(() => {
     toDoList = new ToDoList();
-  document.body.innerHTML = "<li class='to-do-list'></li>";
+  document.body.innerHTML = "<li class='todo-list'></li>";
   localStorage.clear();
 });
 
 test("should add a new task to the list", () => {
   toDoList.addToDo("test");
   document.querySelector(".todo-list").innerHTML = toDoList.renderToDos();
-  const list = document.querySelectorAll(".to-do-list .container");
+  const list = document.querySelectorAll(".todo-list .container");
   expect(list).toHaveLength(1);
 });
-
+test("should remove a task from the list", () => {
+  toDoList.addToDo("test");
+  toDoList.addToDo("test2");
+  toDoList.removeToDo(1);
+  document.querySelector(".todo-list").innerHTML = toDoList.renderToDos();
+  const list = document.querySelectorAll(".todo-list .container");
+  expect(list).toHaveLength(1);
+});
 });
